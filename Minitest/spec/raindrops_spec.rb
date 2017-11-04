deba892e4b8bdd6e87e9f3d7f2b2747d9fcec5c2
msegun <msegun@localhost.localdomain>
2017-11-04 22:51:43 +0100

    Raindrops 8 tests

--- Minitest/spec/raindrops_spec.rb
+++ Minitest/spec/raindrops_spec.rb
@@ -14,5 +14,17 @@ describe Raindrops do
     it 'test_the_sound_for_7_is_Plong' do
       expect(Raindrops.convert(7)).to eq 'Plong'
     end
+    it 'test_the_sound_for_6_is_pling_as_it_has_a_factor_3' do
+      expect(Raindrops.convert(6)).to eq 'Pling'
+    end
+    it 'test_2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base' do
+      expect(Raindrops.convert(8)).to eq '8'
+    end
+    it 'test_the_sound_for_9_is_pling_as_it_has_a_factor_3' do
+      expect(Raindrops.convert(9)).to eq 'Pling'
+    end
+    it 'test_the_sound_for_10_is_plang_as_it_has_a_factor_5' do
+      expect(Raindrops.convert(10)).to eq 'Plang'
+    end
   end
 end
