72dd8a1b6af123a75cbd27bce2c6cad573b559aa
msegun <msegun@localhost.localdomain>
2017-11-05 09:32:27 +0100

    bob with more tests

--- Minitest/spec/bob_test_spec.rb
+++ Minitest/spec/bob_test_spec.rb
@@ -26,5 +26,17 @@ describe Bob do
       remark = 'fffbbcbeab?'
       expect(Bob.hey(remark)).to eq 'Sure.'
     end
+    it 'test_talking_forcefully' do
+      remark = "Let's go make out behind the gym!"
+      expect(Bob.hey(remark)).to eq 'Whatever.'
+    end
+    it 'test_using_acronyms_in_regular_speech' do
+      remark = "It's OK if you don't want to go to the DMV."
+      expect(Bob.hey(remark)).to eq 'Whatever.'
+    end
+    it 'test_forceful_question' do
+      remark = 'WHAT THE HELL WERE YOU THINKING?'
+      expect(Bob.hey(remark)).to eq 'Whoa, chill out!'
+    end
   end
 end
