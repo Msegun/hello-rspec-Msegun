3173acab9aed79d6290d84d82b067d910122d5df
msegun <msegun@localhost.localdomain>
2017-11-05 09:15:20 +0100

    Gigasecond 4 tests rspec

--- Minitest/spec/gigasecond_spec.rb
+++ Minitest/spec/gigasecond_spec.rb
@@ -8,5 +8,11 @@ describe Gigasecond do
     it 'test_second_test_for_date_only_specification_of_time' do
       expect(Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))).to eq Time.utc(2009, 2, 19, 1, 46, 40)
     end
+    it 'test_third_test_for_date_only_specification_of_time' do
+      expect(Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))).to eq Time.utc(1991, 3, 27, 1, 46, 40)
+    end
+    it 'test_full_time_specified' do
+      expect(Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))).to eq Time.utc(2046, 10, 2, 23, 46, 40)
+    end
   end
 end
