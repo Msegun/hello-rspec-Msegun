724c75d2a24fd4b1a75015abcda5b42f20e9eacc
msegun <msegun@localhost.localdomain>
2017-11-10 20:09:50 +0100

    Two tests for qsort

--- ProjectQsort/lib/qsort.rb
+++ ProjectQsort/lib/qsort.rb
@@ -1,34 +1,25 @@
-# Krzysztof Borawski qsort ruby implementation
+# Krzysztof Borawski Quicksort ruby implementation
 class Quicksort
-  def partition_array(array, left, right, pivot_index)
-  pivot_value = array[pivot_index]
-  array[pivot_index], array[right] = array[right], array[pivot_index]
-  store_index = left
-
-  (left..right-1).each do |i|
-    if array[i] < pivot_value
-      array[i], array[store_index] = array[store_index], array[i]
-      store_index = store_index + 1
+  def self.quicksort(array, left, right)
+    if left < right
+      pivot_i = (left + ((right - left)) / 2).to_i
+      new_pivot_i = partition(array, left, right, pivot_i)
+      quicksort(array, left, new_pivot_i - 1)
+      quicksort(array, new_pivot_i + 1, right)
     end
+    array
   end
-
-  array[store_index], array[right] = array[right], array[store_index]
-  return store_index
-end
-
-def inplace_quicksort(array, left, right)
-  if left < right
-    pivot_index = (left + ((right - left) / 2)).to_i
-    new_pivot_index = partition_array(array, left, right, pivot_index)
-    inplace_quicksort(array, left, new_pivot_index - 1)
-    inplace_quicksort(array, new_pivot_index + 1, right)
+  def self.partition(array, left, right, pivot_i)
+    pivot_value = array[pivot_i]
+    array[pivot_i], array[right] = array[right], array[pivot_i]
+    store_i = left
+    (left..right - 1).each do |i|
+      if array[i] < pivot_value
+        array[i], array[store_i] = array[store_i], array[i]
+        store_i += 1
+      end
+    end
+    array[store_i], array[right] = array[right], array[store_i]
+    return store_i
   end
-
-  return array
-end
-  a = [4, -6, 8, 90, -67]
-  p a
-  sort = Quicksort.new
-  sort.inplace_quicksort(a, 0, a.length - 1)
-  p a
 end
--- ProjectQsort/spec/qsort_spec.rb
+++ ProjectQsort/spec/qsort_spec.rb
@@ -1,1 +1,13 @@
 require_relative '../lib/qsort'
+
+describe Quicksort do
+  it 'Beeing given a sorted array' do
+    array = [1, 2, 3, 4, 5, 6]
+    expect(Quicksort.quicksort(array, 0, array.length - 1)).to eq array
+  end
+  it 'Beeing given a inverted array' do
+    array = [6, 5, 4, 3, 2, 1]
+    array2 = [1, 2, 3, 4, 5, 6]
+    expect(Quicksort.quicksort(array, 0, array.length - 1)).to eq array2
+  end
+end
