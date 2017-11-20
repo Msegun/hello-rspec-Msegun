d02eece4ecf97af79af35fc0524960210a4761da
msegun <msegun@localhost.localdomain>
2017-11-20 17:39:46 +0100

    Another 5 tests for my quicksort

--- ProjectQsort/spec/qsort_spec.rb
+++ ProjectQsort/spec/qsort_spec.rb
@@ -10,4 +10,27 @@ describe Quicksort do
     array2 = [1, 2, 3, 4, 5, 6]
     expect(Quicksort.quicksort(array, 0, array.length - 1)).to eq array2
   end
+  it 'Check if a<b == true when a really is lesser than b' do
+    a = 3
+    b = 4
+    expect(Quicksort.less?(a, b)).to eq true
+  end
+  it 'Check if a<b == false when a really is bigger than b' do
+    a = 4
+    b = 3
+    expect(Quicksort.less?(a, b)).to eq false
+  end
+  it 'Check method sorted? when array is sorted' do
+    array = [1, 2, 3, 4, 5, 6]
+    expect(Quicksort.sorted?(array)).to eq true
+  end
+  it 'Check method sorted? when array is !sorted' do
+    array = [6, 2, 1, 4, 5, 6]
+    expect(Quicksort.sorted?(array)).to eq false
+  end
+  it 'Check if array is sorted after sorting' do
+    array = [6, 5, 4, 3, 2, 1]
+    array = Quicksort.quicksort(array, 0, array.length - 1)
+    expect(Quicksort.sorted?(array)).to eq true
+  end
 end
